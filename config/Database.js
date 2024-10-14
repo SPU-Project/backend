2d45794139481a4e040db51365615e7cb9e528c5
hasanganteng123 <hasanabdurrahman123456789@gmail.com>
2024-10-14 22:06:32 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -2,26 +2,10 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const env = process.env.NODE_ENV || "production";
+const env = process.env.NODE_ENV || "development";
 
 const config = {
   development: {
-    username: process.env.PGUSER_DEV,
-    password: process.env.PGPASSWORD_DEV,
-    database: process.env.PGDATABASE_DEV,
-    host: process.env.PGHOST_DEV,
-    dialect: process.env.PGDIALECT_DEV,
-    port: process.env.PGPORT_DEV || 5432,
-  },
-  test: {
-    username: process.env.PGUSER_TEST,
-    password: process.env.PGPASSWORD_TEST,
-    database: process.env.PGDATABASE_TEST,
-    host: process.env.PGHOST_TEST,
-    dialect: process.env.PGDIALECT_TEST,
-    port: process.env.PGPORT_TEST || 5432,
-  },
-  production: {
     host: "localhost",
     username: "postgres",
     password: "hasan123",
